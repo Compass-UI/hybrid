23c3437de82b790f719999cd801cbba0cc96d050
David Shams <david.shams@gmail.com>
2018-09-09 10:25:10 -0500

    modal breadcrum and accordion work

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -11,6 +11,7 @@ angular.module('myApp.view2', ['ngRoute', 'ngResource'])
 }])
 
 .factory('couponData', function($http, $log){
+  console.log('couponData');
   return {
     getCoupon: function() {
       return $http({method: 'GET', url: '../coupon.json'})
@@ -19,17 +20,35 @@ angular.module('myApp.view2', ['ngRoute', 'ngResource'])
 })
 
 .factory('resourceFactory', ['$resource', function($reource){
-  return {
-    getCoupon: function(){
-      return $reource('../coupon.json/:id', {id: '@id'}).get({id:1})
-    }
-  }
+  // let url = '../coupon.json';
+  // let url = 'https://restcountries.eu/rest/v2/name/afghanistan';
+  let url = 'https://restcountries.eu/rest/v2/name/';
+  console.log('resourceFactory');
+  // return {
+    // getCoupon: function(){
+      // return $reource('../coupon.json/:id', {id: '@id'}).get({id:1})
+      // return $reource(`${url}:id`, {id: '@id'}).get({id:'afghanistan', isArray: true})
+      return $reource(`${url}:id`, {id: '@id'}, {
+        query: {
+          method: 'GET',
+          params: {id: 'afghanistan'},
+          isArray: true
+        }
+      })
+    // }
+  // }
 
 }])
 
-// .controller('View2Ctrl', ['$scope', 'couponData', '$log', 'resourceFactory,', function($scope, couponData, $log, resourceFactory) {
-.controller('View2Ctrl', ['$scope', 'couponData', '$log', function($scope, couponData, $log) {
-  // $scope.data = resourceFactory.getCoupon(); // can bind directly to $resource
+.controller('View2Ctrl', ['$scope', 'couponData', '$log', 'resourceFactory', function($scope, couponData, $log, resourceFactory) {
+  // $scope.countries = resourceFactory.getCoupon(); // can bind directly to $resource
+  $scope.countries = resourceFactory.query(); // can bind directly to $resource
+  console.log($scope.countries); // will it work? Yes, looks synchronous but will wait for $resource ( i.e. resourceFactory to return)
+  alert($scope.countries);// will not work
+  resourceFactory.getCoupon().$promise.then(function(data){
+    alert(data);
+    $scope.countries = data
+  })
   couponData.getCoupon().
   success(function(data, status, headers, config){
     console.log(data);
